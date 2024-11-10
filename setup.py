dc6e5ed7c43ed7b050e7a34aaac181be535ac2e6
austincabler13 <austincabler11@gmail.com>
2024-11-10 10:16:08 -0500

    Update pylint configuration and add a newline in setup.py for consistency

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
         'acsecurity=src.scanner:main',
     ],
 },
+
     description='A security scanner for applications.',
     long_description=open("README.md", "r", encoding="utf-8").read(),
     long_description_content_type='text/markdown',
