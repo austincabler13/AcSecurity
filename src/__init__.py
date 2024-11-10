66383b8a5c054d272fc6bef649700977c2043a0e
austincabler13 <austincabler11@gmail.com>
2024-11-10 09:36:42 -0500

    Remove unused virtual environment files and update requirements.txt to include setuptools

--- src/__init__.py
+++ src/__init__.py
@@ -5,7 +5,7 @@ This package provides functionality to scan applications for vulnerabilities
 and code quality issues.
 """
 
-from src.scanner import AcSecurity
+from src.scanner import main as AcSecurity
 __all__ = ['AcSecurity']
 __author__ = 'Austin Cabler'
 __version__ = '1.2.0'
